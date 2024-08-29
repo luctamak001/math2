9d93f374257f45a34e90721126dde355a2867430
Makenley Luctamar <luctamak001@warren.k12.in.us>
2024-08-29 07:18:19 -0400

    done

---
+++ math2.py
@@ -0,0 +1,7 @@
+miles = 5 #pretend we got from user
+minutes = 45 #pretend we got from user
+hours = minutes / 60
+mph = miles / hours
+print("How many miles did you run?", miles)
+print("How many minutes did it take you?", minutes)
+print("Speed in mph:", mph)
